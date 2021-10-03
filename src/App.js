4a6a0fb3002131c26737b178545ae3b889056d6c
Andrew Diles <a_diles@hotmail.com>
2021-10-03 08:08:54 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MasterContainer className="App">
+			Here is some stuff
+    </MasterContainer>
   );
 }
 
 export default App;
+const MasterContainer = styled.div`
+	text-align: center;
+	box-sizing: border-box;
+	
+`
