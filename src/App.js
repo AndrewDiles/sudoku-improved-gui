4f8fa59f40041e7782e3bae4e470ad945c7d4b68
Andrew Diles <a_diles@hotmail.com>
2021-10-04 00:04:05 -0400

    initiatino of theme colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import {useState} from "react";
 import styled from "styled-components";
 
 function App() {
+	const [themeNumber, setThemeNumber] = useState(0);
   return (
-    <MasterContainer className="App">
+    <MasterContainer className="App" themeNumber = {themeNumber}>
 			Here is some stuff
     </MasterContainer>
   );
@@ -13,5 +14,7 @@ export default App;
 const MasterContainer = styled.div`
 	text-align: center;
 	box-sizing: border-box;
-	
+	min-height: 100vh;
+	min-width: 100vw;
+	background: ${p=>`var(--bg${p.themeNumber})`};
 `
