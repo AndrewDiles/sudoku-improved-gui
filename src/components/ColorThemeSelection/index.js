09bb10a55d85acecdb49e3d5c99269a7ca018903
Andrew Diles <a_diles@hotmail.com>
2021-10-08 00:43:44 -0400

    color button backgrounds

--- src/components/ColorThemeSelection/index.js
+++ src/components/ColorThemeSelection/index.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 function ColorThemeSelection({ themeNumber, setThemeNumber }) {
-	console.log(Array.from(Array(10).keys()))
+  const colorNames = ["violet", "classic"];
   return (
     <ButtonsContainer className="App" themeNumber={themeNumber}>
-      {(Array.from(
-        Array(10).keys()).map((number, index) => {
-          return <p key={index}>{number}</p>;
-        })
-      )}
+      {Array.from(Array(2).keys()).map((n, index) => {
+        return (
+          <P key={index} n={n}>
+            {colorNames[n]}
+          </P>
+        );
+      })}
     </ButtonsContainer>
   );
 }
@@ -18,8 +20,44 @@ const ButtonsContainer = styled.div`
   padding-bottom: 20px;
   text-align: center;
   box-sizing: border-box;
-	display: flex;
-	width: 100%;
-	padding: 20px;
-	justify-content: center;
+  display: flex;
+  width: 100%;
+  padding: 20px;
+  justify-content: center;
 `;
+const P = styled.button`
+  margin: 0 20px;
+  padding: 10px 20px;
+  background: ${(p) => `var(--bg-${p.n})`};
+  border: ${(p) => `2px solid var(--border-${p.n})`};
+  color: ${(p) => `var(--text-${p.n})`};
+  /* background-image: linear-gradient(
+      red 5px,
+      transparent 0.1em
+  ),
+  linear-gradient(90deg, transparent 25%, purple 0.1em); */
+  background-image: ${(p) => `linear-gradient(
+	var(--yes-${p.n}) 16%,
+	transparent 1%,
+	transparent 80%,
+	transparent 1%,
+	var(--no-${p.n}) 2%
+),
+linear-gradient(90deg, transparent 75%, var(--bg2-${p.n}) 25%)`};
+:hover {
+	outline: 2px solid ${p => `var(--hover-${p.n})`};
+	cursor: pointer;
+}
+:focus {
+	outline: 2px solid ${p => `var(--focus-${p.n})`};
+}
+:active {
+	outline: 2px solid ${p => `var(--focus-${p.n})`};
+}
+`;
+
+// background-image: ${ p=> `linear-gradient(
+// 	red 5px,
+// 	transparent 0.1em
+// ),
+// linear-gradient(90deg, transparent 20px, var(--bg2-${p.n}) 20px,)`};
