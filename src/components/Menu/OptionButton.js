b668bc20681ce2b5e6af340c17fd5ce79c4bb576
Andrew Diles <a_diles@hotmail.com>
2021-11-02 23:27:34 -0400

    Option Button css fixed

--- src/components/Menu/OptionButton.js
+++ src/components/Menu/OptionButton.js
@@ -27,27 +27,25 @@ export default OptionButton;
 
 const Button = styled.button`
   font-size: 0.7rem;
+  text-align: center;
   margin: 0 5px 10px;
   background: ${(p) => `var(--bg-${p.n})`};
   border: ${(p) => `2px solid var(--border-${p.n})`};
   color: ${(p) => `var(--text-${p.n})`};
+
   /* background-image: linear-gradient(
       red 5px,
       transparent 0.1em
   ),
   linear-gradient(90deg, transparent 25%, purple 0.1em); */
-  background-image: ${(p) => `linear-gradient(
-	var(--yes-${p.n}) 16%,
-	transparent 1%,
-	transparent 80%,
-	transparent 1%,
-	var(--no-${p.n}) 2%
-),
-linear-gradient(90deg, transparent 75%, var(--bg2-${p.n}) 25%)`};
+
   outline: ${(p) => p.isSelected && `3px solid var(--text-${p.n})`};
   transform: scale(1);
+	outline-offset: -2px;
+	text-decoration: ${(p) => p.isSelected && "underline"};
   :hover {
     outline: 2px solid ${(p) => `var(--hover-${p.n})`};
+    background: ${(p) => `var(--bg2-${p.n})`};
     cursor: pointer;
   }
   :focus {
@@ -56,6 +54,7 @@ linear-gradient(90deg, transparent 75%, var(--bg2-${p.n}) 25%)`};
   :active {
     outline: 2px solid ${(p) => `var(--focus-${p.n})`};
     transform: scale(0.95);
+    background: ${(p) => `var(--bg3-${p.n})`};
   }
   transition: transform 0.3s ease-in-out;
   padding: 5px 10px;
@@ -64,3 +63,40 @@ linear-gradient(90deg, transparent 75%, var(--bg2-${p.n}) 25%)`};
     font-size: 1rem;
   }
 `;
+
+// const Button = styled.button`
+//   text-align: center;
+//   background: ${(p) => `var(--bg-${p.themeNumber})`};
+//   border: ${(p) => `2px solid var(--border-${p.themeNumber})`};
+//   color: ${(p) => `var(--text-${p.themeNumber})`};
+//   height: 24px;
+//   width: 24px;
+//   margin: 0 1px;
+//   font-size: 10px;
+//   @media screen and (min-width: 320px) {
+//     height: 30px;
+//     width: 30px;
+//     margin: 0 2px;
+//     font-size: 14px;
+//   }
+//   @media screen and (min-width: 500px) {
+//     height: 45px;
+//     width: 45px;
+//     margin: 0 5px;
+//     font-size: 26px;
+//   }
+//   outline-offset: -2px;
+//   :hover {
+// 		background: ${(p) => `var(--bg2-${p.themeNumber})`};
+//     outline: 2px solid ${(p) => `var(--hover-${p.themeNumber})`};
+//     cursor: pointer;
+//   }
+//   :focus {
+//     outline: 2px solid ${(p) => `var(--focus-${p.themeNumber})`};
+//   }
+//   :active {
+//     outline: 2px solid ${(p) => `var(--focus-${p.themeNumber})`};
+//     font-weight: 900;
+// 		background: ${(p) => `var(--bg3-${p.themeNumber})`};
+//   }
+// `;
